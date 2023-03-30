6e2e38df54ce9ccf20f40ac772d4121b3e693602
princeemeka965 <anyanwue4@gmail.com>
2023-03-30 21:06:42 +0100

    designed homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import Head from "next/dist/shared/lib/head";
-import BzLink from "../../components/BzLink";
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
+import Head from "next/head";
+import BzLink from "../components/BzLink";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function Mobile() {
   return (
